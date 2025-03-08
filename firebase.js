bdc2fbb28c8828a453499481a1e6b75f622a141f
Dégi Nándor <degi.nandor@gmail.com>
2025-03-08 22:05:57 +0200

    fixed security issue

--- firebase.js
+++ firebase.js
@@ -8,13 +8,13 @@ import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, si
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAK83oh6t3mzXIsLoQ3_-l-KTkZJI2K7GM",
-  authDomain: "greedy-shop.firebaseapp.com",
-  projectId: "greedy-shop",
-  storageBucket: "greedy-shop.appspot.com",
-  messagingSenderId: "1091798060691",
-  appId: "1:1091798060691:web:bebd5e3bca72d56e158766",
-  measurementId: "G-B22E6T6WPL"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
